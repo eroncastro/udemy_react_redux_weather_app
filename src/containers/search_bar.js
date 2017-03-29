e36fe3f596f4c83b2a7461fd975f2d0a0d224a96
Eron <eroncastro@gmail.com>
2017-03-29 07:49:39 -0300

    Improves SearchBar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+/*
+  Controlled field - form element where the value of the input
+  is set by the state of our component.
+*/
+
+
 export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
